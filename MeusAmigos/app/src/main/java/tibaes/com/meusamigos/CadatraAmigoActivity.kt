3edea04a3b7046476c49749fa6a7652ca4e39202
julianatibaes <tibaes.juliana@gmail.com>
2018-10-25 21:49:03 -0300

    adição de meus amigos

---
+++ MeusAmigos/app/src/main/java/tibaes/com/meusamigos/CadatraAmigoActivity.kt
@@ -0,0 +1,12 @@
+package tibaes.com.meusamigos
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CadatraAmigoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadatra_amigo)
+    }
+}
